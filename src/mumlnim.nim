542cd7730895c6da208992ad1e05a5d62c631fe8
Mutsuha Asada <momiimt@gmail.com>
2021-08-02 07:47:34 +0900

    👍 Add: implement array-index

--- src/mumlnim.nim
+++ src/mumlnim.nim
@@ -1,11 +1,18 @@
 import json
 
 type
-  mumlVideo* = object
-    path*: string
-    frame*: tuple[start: float, `end`: float]
-    video*: mumlVideo_Video
-    audio*: mumlVideo_Audio
+  mumlNode* = JsonNode
+
+  mumlKind* = enum
+    mumlVideo
+
+  mumlObject* = object
+    case kind: mumlKind
+    of mumlVideo:
+      path*: string
+      frame*: tuple[start: float, `end`: float]
+      video*: mumlVideo_Video
+      audio*: mumlVideo_Audio
 
   mumlVideo_Video* = object
     frame*: tuple[start: float, `end`: float]
@@ -20,50 +27,56 @@ type
 proc removeDoubleQuotation (str: string): string =
   result = str[0..str.len-1]
 
-proc muml* (path: string): JsonNode =
+proc muml* (path: string): mumlNode =
   let json = path.readFile().parseJson
   if not json.hasKey("muml"):
     raise newException(Exception, "no muml")
   result = json["muml"]
 
-proc content* (muml: JsonNode): JsonNode =
+proc content* (muml: mumlNode): mumlNode =
   if not muml.hasKey("content"):
     raise newException(Exception, "no content")
   result = muml["content"]
 
-proc getVideo* (content: JsonNode): mumlVideo =
-  result = mumlVideo()
-  
-  for tag in content.items:
-    if not tag.hasKey("type"):
-      raise newException(Exception, "no type tag")
-    case tag["type"].getStr.removeDoubleQuotation:
+proc type (muml: mumlNode): string =
+  if not muml.hasKey("type"):
+    raise newException(Exception, "no type tag")
+  result = muml["type"].getStr.removeDoubleQuotation
+
+proc getVideo (muml: mumlNode): mumlObject =
+  result = mumlObject(kind: mumlVideo)
+  for key, val in muml.pairs:
+    case key:
+    of "path":
+      result.path = val.getStr.removeDoubleQuotation
+    of "frame":
+      result.frame.start = val["start"].getFloat
+      result.frame.`end` = val["end"].getFloat
     of "video":
-      for key, val in tag.pairs:
-        case key:
-        of "path":
-          result.path = val.getStr.removeDoubleQuotation
+      for key2, val2 in val.pairs:
+        case key2:
         of "frame":
-          result.frame.start = val["start"].getFloat
-          result.frame.`end` = val["end"].getFloat
-        of "video":
-          for key2, val2 in val.pairs:
-            case key2:
-            of "frame":
-              result.video.frame.start = val2["start"].getFloat
-              result.video.frame.`end` = val2["end"].getFloat
-            of "position":
-              result.video.position.x = val2["x"].getFloat
-              result.video.position.y = val2["y"].getFloat
-            of "scale":
-              result.video.scale.width = val2["width"].getFloat
-              result.video.scale.height = val2["height"].getFloat
-            of "rotate":
-              result.video.rotate = val2.getFloat
-            of "opacity":
-              result.video.opacity = val2.getFloat
-        of "audio":
-          for key2, val2 in val.pairs:
-            case key2:
-            of "volume":
-              result.audio.volume = val2.getFloat
+          result.video.frame.start = val2["start"].getFloat
+          result.video.frame.`end` = val2["end"].getFloat
+        of "position":
+          result.video.position.x = val2["x"].getFloat
+          result.video.position.y = val2["y"].getFloat
+        of "scale":
+          result.video.scale.width = val2["width"].getFloat
+          result.video.scale.height = val2["height"].getFloat
+        of "rotate":
+          result.video.rotate = val2.getFloat
+        of "opacity":
+          result.video.opacity = val2.getFloat
+    of "audio":
+      for key2, val2 in val.pairs:
+        case key2:
+        of "volume":
+          result.audio.volume = val2.getFloat
+
+proc `[]`* (muml: mumlNode, index: int): mumlObject {.inline.} =
+  let target_node = muml.elems[index]
+  echo target_node.type
+  result = case target_node.type:
+    of "video": getVideo(target_node)
+    else: raise newException(Exception, "not found tag")
