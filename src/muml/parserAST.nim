29bc3e13ec26dc93a842b0d6abb06f6949d39a67
Mutsuha Asada <momiimt@gmail.com>
2022-08-02 14:43:37 +0900

    👍 add: support for int16, int32, int64, bool

--- src/muml/parserAST.nim
+++ src/muml/parserAST.nim
@@ -25,6 +25,30 @@ proc getInt8ParserAST* (deserializeKey: string, keyValueID: int): NimNode =
       `resultElement`.`key` = int8(getInt(`val`))
   )
 
+proc getInt16ParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      `resultElement`.`key` = int16(getInt(`val`))
+  )
+
+proc getInt32ParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      `resultElement`.`key` = int32(getInt(`val`))
+  )
+
+proc getInt64ParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      `resultElement`.`key` = int64(getInt(`val`))
+  )
+
 proc getFloatParserAST* (deserializeKey: string, keyValueID: int): NimNode =
   let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
   result = nnkOfBranch.newTree(
@@ -49,6 +73,14 @@ proc getEnumParserAST* (typeName, deserializeKey: string, keyValueID: int): NimN
       `resultElement`.`key` = parseEnum[`typeName`](getStr(`val`).removeDoubleQuotation)
   )
 
+proc getBoolParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      `resultElement`.`key` = getBool(`val`)
+  )
+
 proc getFloatSequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode =
   let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
   result = nnkOfBranch.newTree(
@@ -67,6 +99,42 @@ proc getIntSequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode
         `resultElement`.`key`.add jsonArrayElement.getInt
   )
 
+proc getInt8SequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      for jsonArrayElement in `val`:
+        `resultElement`.`key`.add int8(jsonArrayElement.getInt)
+  )
+
+proc getInt16SequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      for jsonArrayElement in `val`:
+        `resultElement`.`key`.add int16(jsonArrayElement.getInt)
+  )
+
+proc getInt32SequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      for jsonArrayElement in `val`:
+        `resultElement`.`key`.add int32(jsonArrayElement.getInt)
+  )
+
+proc getInt64SequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      for jsonArrayElement in `val`:
+        `resultElement`.`key`.add int64(jsonArrayElement.getInt)
+  )
+
 proc getStringSequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode =
   let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
   result = nnkOfBranch.newTree(
@@ -74,4 +142,13 @@ proc getStringSequenceParserAST* (deserializeKey: string, keyValueID: int): NimN
     quote do:
       for jsonArrayElement in `val`:
         `resultElement`.`key`.add jsonArrayElement.getStr.removeDoubleQuotation
+  )
+
+proc getBoolSequenceParserAST* (deserializeKey: string, keyValueID: int): NimNode =
+  let (val, resultElement, key) = toIdent(deserializeKey, keyValueID)
+  result = nnkOfBranch.newTree(
+    newLit(deserializeKey),
+    quote do:
+      for jsonArrayElement in `val`:
+        `resultElement`.`key`.add jsonArrayElement.getBool
   )
