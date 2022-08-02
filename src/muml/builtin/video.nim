dec26ea877ccc45036a1054e9b91c7bd5c9ad039
Mutsuha Asada <momiimt@gmail.com>
2022-08-02 13:17:11 +0900

    👍 add: Video (built-in)

---
+++ src/muml/builtin/video.nim
@@ -0,0 +1,20 @@
+import types, ../builder
+
+type
+  mumlVideoFrame* = object
+    start*: int
+    stop*: int
+
+  mumlFilter* = object
+
+  Video* = ref object of mumlRootElement
+    path*: string
+    videoFrame*: mumlVideoFrame
+    position*: Animation[mumlPosition]
+    scale*: Animation[mumlScale]
+    rotate*: Animation[float]
+    opacity*: Animation[float]
+    # filters*: seq[mumlFilter]
+    # audio*: mumlAudio
+
+mumlBuilder(Video)
