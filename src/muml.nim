8ef00e0c90056770c2a34468d58ec8499f8b6d7b
momeemt <momiimt@gmail.com>
2022-08-02 09:30:51 +0900

    🚧 wip: Muml type

--- src/muml.nim
+++ src/muml.nim
@@ -1,48 +1,39 @@
-import json, muml/[types, utils, video, rectangle, header, text, triangle], uuids
-export types, utils, video, rectangle, header, text, uuids, triangle
+import json, muml/[types, utils, video, rectangle, header, text, triangle, builder], uuids
+export types, utils, video, rectangle, header, text, uuids, triangle, builder
 
-proc muml* (path: string): mumlNode =
-  let json = path.readFile().parseJson
+type
+  Muml* = object
+    header: JsonNode
+    contents: JsonNode
+
+proc muml* (json: JsonNode): Muml =
   if not json.hasKey("muml"):
     raise newException(Exception, "no muml")
-  result = json["muml"]
-
-proc muml* (json: JsonNode): mumlNode =
-  if not json.hasKey("muml"):
-    raise newException(Exception, "no muml")
-  result = json["muml"]
-
-proc header* (muml: mumlNode): mumlNode =
-  if not muml.hasKey("header"):
+  if not json["muml"].hasKey("header"):
     raise newException(Exception, "no header")
-  result = muml["header"]
+  if not json["muml"].hasKey("contents"):
+    raise newException(Exception, "no contents")
 
-proc content* (muml: mumlNode): mumlNode =
-  if not muml.hasKey("content"):
-    raise newException(Exception, "no content")
-  result = muml["content"]
+  result.header = json["muml"]["header"]
+  result.contents = json["muml"]["contents"]
 
 proc type (muml: mumlNode): string =
   if not muml.hasKey("type"):
     raise newException(Exception, "no type tag")
   result = muml["type"].getStr.removeDoubleQuotation
 
-proc `[]`* (muml: mumlNode, index: int): mumlObject {.inline.} =
-  let target_node = muml.elems[index]
-  result = case target_node.type:
-    of "video": getVideo(target_node)
-    else: raise newException(Exception, "not found tag")
+iterator default_content* (muml: Muml): mumlRootObj =
+  for element in muml.contents:
+    var mumlObj = case element.type:
+      
 
 iterator element* (muml: mumlNode): mumlObject =
   for elem in muml.items:
     var mumlObj = case elem.type:
       of "video": getVideo(elem)
+      of "triangle": getTriangle(elem)
       of "rectangle": getRectangle(elem)
       of "text": getText(elem)
       else: raise newException(Exception, "not found tag")
     mumlObj.uuid = genUuid()
     yield mumlObj
-
-iterator deserialize* (muml: JSONNode): mumlRootObj =
-  for element in muml.items:
-    discard
