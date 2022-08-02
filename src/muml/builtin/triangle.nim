8ef56bcbeaae049148a03d0c01169e43c894bb0d
Mutsuha Asada <momiimt@gmail.com>
2022-08-02 13:03:48 +0900

    👍 change: publish members and change type name

--- src/muml/builtin/triangle.nim
+++ src/muml/builtin/triangle.nim
@@ -1,10 +1,10 @@
 import types, ../builder
 
 type
-  mumlTriangle* = ref object of mumlRootElement
-    position: Animation[mumlPosition]
-    scale: Animation[mumlScale]
-    rotate: Animation[float]
-    opacity: Animation[float]
+  Triangle* = ref object of mumlRootElement
+    position*: Animation[mumlPosition]
+    scale*: Animation[mumlScale]
+    rotate*: Animation[float]
+    opacity*: Animation[float]
 
-mumlBuilder(mumlTriangle)
+mumlBuilder(Triangle)
